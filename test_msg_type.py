13b812368065d5f2d850749952f4fdfd4316632d
kayzeng <kayzeng@users.noreply.github.com>
2018-05-06 07:50:35 +0800

    图片和文字消息的防撤回

--- test_msg_type.py
+++ test_msg_type.py
@@ -10,12 +10,40 @@ from itchat.content import *
 msg_dict = {}
 
 
-# 收集群文本消息到字典里
-@itchat.msg_register([PICTURE], isGroupChat=True)
+current_time = time.time()
+current_time_int = int(current_time)
+
+
+@itchat.msg_register([TEXT], isGroupChat=True)
 def _(msg):
     msg_dict[msg.MsgId] = msg
-    print(msg_dict)
+    # print(msg_dict)
+    print(current_time_int)
+    print(msg['CreateTime'])
+    delta = msg['CreateTime'] - current_time_int
+    print(delta)
+    print(type(delta))
 
+
+
+# msg_dict@itchat.msg_register([PICTURE], isGroupChat=True)
+# def download_files(msg):
+#
+#     # 下载图片到本地
+#     msg.download(msg.fileName)
+#
+#     # 图片消息存到字典
+#     msg_dict[msg.MsgId] = msg
+#     print(msg_dict)
+
+
+
+
+# 发送文本消息
+# itchat.send_msg(msg='Text Message', toUserName=None)
+
+# 发送图片消息
+# send_img(fileDir, toUserName=None)
 
 
 itchat.auto_login(enableCmdQR=2, hotReload=True)
--- wx_anti_revoke.py
+++ wx_anti_revoke.py
@@ -4,28 +4,32 @@ import shutil
 import itchat
 import time
 from itchat.content import *
+from threading import Timer
 
 
-# 定义一个字典，保存消息的信息
+# 定义一个字典，保存消息体
 msg_dict = {}
 
 
-# 新建一个文件夹，保存消息的图片
-msg_file_tmp_dir = "/Users/kayzeng/Downloads/itchat/msg_file_tmp/"
-if not os.path.exists(msg_file_tmp_dir):
-    os.mkdir(msg_file_tmp_dir)
-
-
-# 收集群文本消息到字典里
+# 收集群文本消息
 @itchat.msg_register(itchat.content.TEXT, isGroupChat=True)
 def _(msg):
+    #存到字典
     msg_dict[msg.MsgId] = msg
+    # print(msg_dict)
 
 
-# 收集群图片消息到字典里
-@itchat.msg_register(itchat.content.PICTURE, isGroupChat=True)
-def _(msg):
+# 收集群图片消息
+@itchat.msg_register([PICTURE], isGroupChat=True)
+def download_files(msg):
+
+    # 下载图片到本地
+    msg.download(msg.fileName)
+
+    # 存到字典
     msg_dict[msg.MsgId] = msg
+    # print(msg_dict)
+
 
 
 # 捕获撤回消息的提醒，查找旧消息并回复
@@ -40,15 +44,51 @@ def _(msg):
         # 原消息是文本消息
         if old_msg['Type'] == 'Text':
             old_msg_text = msg_dict[old_msg_id]['Text']
-            itchat.send(msg=msg['ActualNickName']+'：'+old_msg_text, toUserName=msg['FromUserName'])
+            itchat.send(msg=msg['ActualNickName']+' 刚才发过这条消息：'+old_msg_text, toUserName=msg['FromUserName'])
 
         # 原消息是图片消息
         elif old_msg['Type'] == 'Picture':
 
+            # 发送文本消息
+            itchat.send_msg(msg=msg['ActualNickName']+' 刚才发过这张图片', toUserName=msg['FromUserName'])
+
+            # 发送图片消息
+            old_msg_img_file_dir = msg_dict[old_msg_id]['FileName']
+            itchat.send('@img@%s' % old_msg_img_file_dir, toUserName=msg['FromUserName'])
+
+
 
 # 定时干掉字典里和文件夹里的超时消息备份
+out_date_msg_dict = []
+
+def delete_out_date_msg():
+    for m in msg_dict:
+
+        current_time = time.time()
+        current_time_int = int(current_time)
+
+        if (current_time_int - msg_dict[m]['CreateTime']) > 130:
+
+            out_date_msg_dict.append(m)
+
+    for n in out_date_msg_dict:
+
+        if msg_dict[n]['Type'] == 'Text':
+            msg_dict.pop(n)
+
+        elif msg_dict[n]['Type'] == 'Picture':
+            os.remove(msg_dict[n]['FileName'])
+            msg_dict.pop(n)
+
+        # print(msg_dict)
+        out_date_msg_dict.clear()
 
 
+    t = Timer(10, delete_out_date_msg)
+    t.start()
+
+delete_out_date_msg()
 
 itchat.auto_login(enableCmdQR=2, hotReload=True)
 itchat.run()
+
